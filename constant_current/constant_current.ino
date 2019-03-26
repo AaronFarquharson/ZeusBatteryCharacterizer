ecf1bcd0eeef097d38a6b75ebf51dcbd4d7a33ca
Aaron Farquharson <aaron.farquharson9@gmail.com>
2019-03-26 13:07:21 -0600

    End of 2019-03-26 meeting

--- constant_current/constant_current.ino
+++ constant_current/constant_current.ino
@@ -13,23 +13,28 @@ void setup() {
 }
 
 void loop() {
-  analogWrite(PWM_PIN, 200);
-  delay(100);
-  Serial.print("Restitor V: ");
+  double batVolt = readBatteryVoltage();
+  if((batVolt > 3.35)) {
+    setCurrent(10); //set to 4A
+  }
+  delay(500);
+  Serial.print("Discharge: \t");
+  Serial.print("Resistor A: ");
+  Serial.print(readResistorCurrent());
+  Serial.print("\tResistor V: ");
   Serial.print(readResistorVoltage());
   Serial.print("\tBattery V: ");
-  Serial.println(readBatteryVoltage());
+  Serial.println(batVolt);
   
   delay(500);
 
-  analogWrite(PWM_PIN, 0);
+  setCurrent(0); //set to 0V
 
   delay(1500); //Need time for the voltage on the battery to recover after the heavy current draw
-  
+
+  Serial.print("No Discharge: \t");
   Serial.print("\tBattery V: ");
   Serial.println(readBatteryVoltage());
-
-  delay(100);
 }
 
 /* Needed functions:
@@ -52,7 +57,7 @@ void loop() {
  }
 
  void setCurrent(double current){
-  setPWMVoltage(current * 2); // This function needs testing! Coefficient needs to be changed
+  setPWMVoltage(current * 1); // This function needs testing! Coefficient needs to be changed
  }
 
  void setPWMVoltage(double voltage){
